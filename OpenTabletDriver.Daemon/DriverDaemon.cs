9d826961b5e466698b59e2c2d7b9c242a8bceed1
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2026-02-06 18:33:37 +0100

    (minor) DriverDaemon: Reorder logging/initialization so that mouse stuff stays together

--- OpenTabletDriver.Daemon/DriverDaemon.cs
+++ OpenTabletDriver.Daemon/DriverDaemon.cs
@@ -511,12 +511,6 @@ namespace OpenTabletDriver.Daemon
                 Log.Write(group, $"Express Key Bindings: " + string.Join(", ", bindingHandler.AuxButtons.Select(b => b.Value?.Binding)));
             }
 
-            if (settings.MouseButtons != null && settings.MouseButtons.Any(b => b?.Path != null))
-            {
-                SetBindingHandlerCollectionSettings(bindingServiceProvider, settings.MouseButtons, bindingHandler.MouseButtons, tabletReference);
-                Log.Write(group, $"Mouse Button Bindings: [" + string.Join("], [", bindingHandler.MouseButtons.Select(b => b.Value?.Binding)) + "]");
-            }
-
             for (int wheelIndex = 0; wheelIndex < settings.WheelBindings.Count; wheelIndex++)
             {
                 var wheelBindingSetting = settings.WheelBindings[wheelIndex];
@@ -554,6 +548,12 @@ namespace OpenTabletDriver.Daemon
 
                 if (counterClockwiseRotation.Binding != null)
                     Log.Write(group, $"Wheel {wheelIndex + 1} Counter-Clockwise Rotation: [{counterClockwiseRotation.Binding}]@{counterClockwiseRotation.ActivationThreshold}°");
+            }
+
+            if (settings.MouseButtons != null && settings.MouseButtons.Any(b => b?.Path != null))
+            {
+                SetBindingHandlerCollectionSettings(bindingServiceProvider, settings.MouseButtons, bindingHandler.MouseButtons, tabletReference);
+                Log.Write(group, $"Mouse Button Bindings: [" + string.Join("], [", bindingHandler.MouseButtons.Select(b => b.Value?.Binding)) + "]");
             }
 
             var scrollUp = bindingHandler.MouseScrollUp = new BindingState
